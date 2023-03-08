2e8ded0ee9a144702f1985145f784b1ffb5d73f0
Jake Hiney <hiney.jake@outlook.com>
2023-03-08 14:10:47 -0600

    move appshell to docs

---
+++ src/routes/docs/+layout.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import { AppRail } from '@skeletonlabs/skeleton';
+	import { AppRailTile } from '@skeletonlabs/skeleton';
+	import { writable } from 'svelte/store';
+
+	import Icon from '../../icons/icon.svelte';
+
+	const storeValue = writable(1);
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">hello</svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft"
+		><AppRail selected={storeValue}>
+			<svelte:fragment slot="lead" />
+			<AppRailTile label="Tile 1" value={1} />
+			<AppRailTile label="Tile 2" value={2}>
+				<Icon icon="bell" />
+			</AppRailTile>
+			<AppRailTile label="Tile 3" value={3}>
+				<i class="fa-solid fa-image text-2xl" />
+			</AppRailTile>
+			<svelte:fragment slot="trail">
+				<AppRailTile label="Tile footer" value={4}>
+					<i class="fa-solid fa-image text-2xl" />
+				</AppRailTile>
+			</svelte:fragment>
+		</AppRail></svelte:fragment
+	>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
